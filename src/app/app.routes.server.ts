ca86a72eb3effd0b6145768cc4e8f5c2ed024ba4
alex.newman@intapp.com <alex.newman@intapp.com>
2026-02-17 09:16:57 +0000

    fix build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'tag-invalidation',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'lazy-demo',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'cache-control',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
